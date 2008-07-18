939044fa55d04638cb84d880918cd1e7930049c3
cbmdodo <cbmdodo@a948822d-4852-0410-9fb4-71b3ed5ff8a0>
2008-07-18 11:20:38 +0000

    Se ha puesto en incorporajugadores.html un formulario que envia el código fuente de la plantilla de hattrick a incorporajugadores.php el cual lo parseará para incorporar los datos a la BD(cuando esté) de los jugadores de cada equipo. Tipo juveniles de tommatrick

--- thepiso.php
+++ thepiso.php
@@ -15,11 +15,37 @@
 	<div class="logoequipo">
     	<div class="imagenlogoequipo">
     		<img src="images/BannerThepiso.png" alt="ThePiso">        </div>
-        <div class="nombreEquipoLogo">
+<div class="nombreEquipoLogo">
             	<p class="listaequipo"><h4>ThePiso</h4></p>
                 <p class="listaequipo Estilo1">España IX.933</p>
                 <p class="listaequipo">Estadio Coliseo Alejandro Casona (12010)</p>
         </div>
-    </div>
+        <?php
+			function get_dato($string, $start, $end){
+			$string = " ".$string;
+			$ini = strpos($string,$start);
+			if ($ini == 0) return "";
+				$ini += strlen($start);   
+				$len = strpos($string,$end,$ini) - $ini;
+				return substr($string,$ini,$len);
+			}
+			// Intento de parsear una plantilla de un equipo de ht
+			$texto='<A HREF="playerDetails.asp?playerID=159191430">Camilo  Vaz</A> &nbsp;<a href="bookmarks.aspx?actionType=add&bookmarkTypeID=2&ObjectID=159191430"><img src="img.axd?res=Icons&img=bookmarks.gif" title="Añadir este jugador a tus favoritos"/></a>&nbsp;<BR>';
+			$id= get_dato($texto,'playerDetails.asp?playerID=','">');
+			echo '<br>';
+			echo '<br>';
+			echo '<br>';
+			echo '<br>';
+			echo '<br>';
+			echo '<br>';
+			echo '<br>';
+			echo '<br>';
+			echo '<br>';
+			echo '<br>';
+			echo '<br>';
+			echo $id;
+			?>
+            <a href="incorporajugadores.html" target="_blank">añadeJugadores</a>
+</div>
 </body>
 </html>
